15942ab32f2af0fd24bc3ffe1a7c5ca5ef0e6ce7
mohamed Ail <159685876+mo2002222@users.noreply.github.com>
2026-01-23 20:20:12 +0200

    Change CORS origin to production URL

--- back-end/index.js
+++ back-end/index.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 dotenv.config();  
 app.use(cors({  
-    origin: 'http://localhost:5173',
+    origin: 'https://top-talent-six.vercel.app/',
     credentials: true,   
 })); 
 app.use(express.json()); 
